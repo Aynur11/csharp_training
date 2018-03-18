2f91cfa593c3e0e23a48f058dc12b8fd10036f0d
Aynur <ainur345@yandex.ru>
2018-03-19 00:09:23 +0500

    Добавил тест проверки информации на главной странице

--- addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ContactHelper.cs
@@ -20,6 +20,45 @@ namespace addressbook_web_tests
             return this;
         }
 
+        public ContactData GetContactInformationFromEditForm(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            InitContactModification(index + 1);
+            string firstName = driver.FindElement(By.Name("firstname")).GetAttribute("value");
+            string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
+            string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+            string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
+            string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
+            string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+            string email = driver.FindElement(By.Name("email")).GetAttribute("value");
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                HomePhone = homePhone,
+                MobilePhone = mobilePhone,
+                WorkPhone = workPhone,
+                Email = email
+            };
+        }
+
+        public ContactData GetContactInformationFromTable(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index].FindElements(By.TagName("td"));
+            string firstName = cells[2].Text;
+            string lastName = cells[1].Text;
+            string address = cells[3].Text;
+            string allPhones = cells[5].Text;
+            string email = cells[4].Text;
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                Email = email,
+                AllPhones = allPhones
+            };
+        }
+
         internal List<ContactData> GetContactsList()
         {
             if (contactCache == null)
---
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactInformationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace addressbook_web_tests.Tests
+{
+    [TestFixture]
+    class ContactInformationTests : AuthTestBase
+    {
+        [Test]
+        public void ContactInformationTest()
+        {
+            ContactData fromTable = manager.Contacts.GetContactInformationFromTable(1);
+            ContactData fromForm = manager.Contacts.GetContactInformationFromEditForm(1);
+            Assert.AreEqual(fromTable, fromForm);
+            Assert.AreEqual(fromTable.Address, fromForm.Address);
+            Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
+
+        }
+    }
+}

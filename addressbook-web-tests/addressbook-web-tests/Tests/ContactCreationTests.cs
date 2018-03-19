9d2b83ce368c79a00c5cbadf2e5756ddc88a6306
Aynur <ainur345@yandex.ru>
2018-03-20 00:43:24 +0500

    Параметризованные тесты для групп и контактов

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -12,17 +12,33 @@ namespace addressbook_web_tests
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        [Test]
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(30), GenerateRandomString(30))
+                {
+                    Address = GenerateRandomString(100),
+                    HomePhone = GenerateRandomString(100),
+                    MobilePhone = GenerateRandomString(100),
+                    WorkPhone = GenerateRandomString(100),
+                    Email = GenerateRandomString(100)
+                });
+            }
+            return contacts;
+        }
+        [Test, TestCaseSource("RandomContactDataProvider")]
+        public void ContactCreationTest(ContactData contact)
         {
             List<ContactData> oldContacts = manager.Contacts.GetContactsList();
-            ContactData entryData = new ContactData("gIvan", "glumDIvanov");
-            manager.Contacts.Create(entryData);
+            //ContactData entryData = new ContactData("gIvan", "glumDIvanov");
+            manager.Contacts.Create(contact);
             List<ContactData> newContacts = manager.Contacts.GetContactsList();
 
             Assert.AreEqual(oldContacts.Count + 1, manager.Contacts.GetContactCount());
 
-            oldContacts.Add(entryData);
+            oldContacts.Add(contact);
           
             oldContacts.Sort();
             newContacts.Sort();
